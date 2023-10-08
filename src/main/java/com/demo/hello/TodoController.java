c2039bfa359f34ef40770eaba747ea6de9419a01
tanishka <tanishka.k.kothari@gmail.com>
2023-10-08 17:17:05 +0530

    final changes done

--- src/main/java/com/demo/hello/TodoController.java
+++ src/main/java/com/demo/hello/TodoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class TodoController {
 
     @Autowired
     private TodoService todoService;
